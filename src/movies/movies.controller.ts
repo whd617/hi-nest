0ba01677f7dc7250bcd4846d4bd4ba0df7b6ae9e
Jonghyeon Choi <whd617@naver.com>
2024-04-10 17:19:27 +0900

    test/e2e testing 완료

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -25,7 +25,6 @@ export class MoviesController {
 
   @Get(':id')
   getOne(@Param('id') movieId: number): Movie {
-    console.log(typeof movieId);
     return this.moviesService.getOne(movieId);
   }
 
